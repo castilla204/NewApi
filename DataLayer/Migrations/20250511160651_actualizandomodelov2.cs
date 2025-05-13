e6f11a8535f19321e8d20bea022bfff5664aa461
castilla204 <tu_email@ejemplo.com>
2025-05-13 20:45:17 +0200

    subiendoo

---
+++ DataLayer/Migrations/20250511160651_actualizandomodelov2.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class actualizandomodelov2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Searches_Users_ExpertId",
+                table: "Searches");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Searches_ExpertId",
+                table: "Searches");
+
+            migrationBuilder.DropColumn(
+                name: "ExpertId",
+                table: "Searches");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExpertId",
+                table: "Searches",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Searches_ExpertId",
+                table: "Searches",
+                column: "ExpertId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Searches_Users_ExpertId",
+                table: "Searches",
+                column: "ExpertId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

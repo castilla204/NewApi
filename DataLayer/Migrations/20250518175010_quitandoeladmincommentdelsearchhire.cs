2b7f249046f1391c97302c9f7dfaaebb0cab9b7d
castilla204 <tu_email@ejemplo.com>
2025-05-18 22:37:57 +0200

    subiendo

---
+++ DataLayer/Migrations/20250518175010_quitandoeladmincommentdelsearchhire.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class quitandoeladmincommentdelsearchhire : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdminComments",
+                table: "Disputes");
+
+            migrationBuilder.DropColumn(
+                name: "Resolution",
+                table: "Disputes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdminComments",
+                table: "Disputes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resolution",
+                table: "Disputes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

ad95d092661400b35ea344db6e65caddb3779489
castilla204 <tu_email@ejemplo.com>
2025-07-22 22:29:54 +0200

    añadiendo información extra a los search

--- DataLayer/Models/DTOs/SearchHireDto.cs
+++ DataLayer/Models/DTOs/SearchHireDto.cs
@@ -1,6 +1,12 @@
 ﻿namespace newApi.DataLayer.Models.DTOs
 {
-
+    public class SearchHireDto
+    {
+        public int Id { get; set; }
+        public int? ExpertId { get; set; }
+        public string Status { get; set; }
+        public UserDto? Expert { get; set; }
+    }
     public class CreateSearchHireDto
     {
         public int SearchServiceId { get; set; }
@@ -28,4 +34,20 @@
     {
         public string Status { get; set; }
     }
+    public class SearchServiceResponseDto
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int ServiceTypeId { get; set; } // Added
+        public string ServiceTypeName { get; set; } // Added
+        public decimal Price { get; set; }
+        public string Conditions { get; set; }
+        public int DurationInHours { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<string> ImageUrls { get; set; }
+        public ExpertProfileDto? Expert { get; set; }
+    }
+
+
+
 }

d9050f611baecc9d78ae4336ecc8e99a94dba881
castilla204 <tu_email@ejemplo.com>
2025-08-14 14:49:31 +0200

    mejorando proceso compra

--- DataLayer/Models/DTOs/CreateSearchDto.cs
+++ DataLayer/Models/DTOs/CreateSearchDto.cs
@@ -9,6 +9,7 @@ namespace newApi.DataLayer.Models.DTOs
         public string? Description { get; set; }
         public bool IsActive { get; set; }
         public DateTime StartDate { get; set; }
+        public int ServiceId { get; set; }
     }
 
 

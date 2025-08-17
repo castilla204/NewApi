ba55a58d8a81c78254a43fb291481b0751593af6
castilla204 <tu_email@ejemplo.com>
2025-08-17 22:57:09 +0200

    subiendooo

--- DataLayer/Models/PostGresModels/Message.cs
+++ DataLayer/Models/PostGresModels/Message.cs
@@ -1,16 +1,21 @@
 ﻿namespace newApi.DataLayer.Models.PostGresModels
 {
+
+
     public class Message
     {
         public int Id { get; set; }
         public int ConversationId { get; set; } // Ties the message to a conversation
         public int SenderId { get; set; } // The user (client or expert) who sent the message
-        public string Content { get; set; } // The message text
+        public string? Content { get; set; } // The message text
         public DateTime SentAt { get; set; } = DateTime.UtcNow;
         public bool IsRead { get; set; } = false; // Tracks if the message has been read
+        public string? LocationLatitude { get; set; } // Latitude for shared location
+        public string? LocationLongitude { get; set; } // Longitude for shared location
 
         // Navigation properties
         public virtual Conversation Conversation { get; set; }
         public virtual User Sender { get; set; }
+        public virtual ICollection<MessageAttachment> Attachments { get; set; } = new List<MessageAttachment>();
     }
 }
--- DataLayer/Models/PostGresModels/SearchHire.cs
+++ DataLayer/Models/PostGresModels/SearchHire.cs
@@ -6,7 +6,7 @@ namespace newApi.DataLayer.Models.PostGresModels
     {
         public int Id { get; set; }
         public int ClientId { get; set; }
-        public int? ExpertId { get; set; } 
+        public int? ExpertId { get; set; }
         public int SearchServiceId { get; set; }
         public int SearchId { get; set; }
         public string Status { get; set; }
@@ -17,11 +17,12 @@ namespace newApi.DataLayer.Models.PostGresModels
         public DateTime? CompletionDeadline { get; set; }
         public bool? ClientApproved { get; set; }
         public virtual User Client { get; set; }
-        public virtual User? Expert { get; set; } 
+        public virtual User? Expert { get; set; }
         public virtual SearchService SearchService { get; set; }
         public virtual Search Search { get; set; }
         public virtual ICollection<Dispute> Disputes { get; set; }
         public virtual ICollection<Conversation> Conversations { get; set; } = new List<Conversation>();
+        public virtual ICollection<SearchHireDeliverable> Deliverables { get; set; } = new List<SearchHireDeliverable>();
     }
 
 }
